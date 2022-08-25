62c63497cbafad00164f115cf83821b701d13455
sunghyun <cornsworld@naver.com>
2022-08-25 22:55:11 +0900

    init

---
+++ src/main/java/com/example/dice/Main.java
@@ -0,0 +1,12 @@
+package com.example.dice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
